fb3c7774ff2d7f5a073f286e9a2d151831eba864
rucocohero <cocoru@gmx.de>
2017-05-29 20:12:51 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "basicBot";
+    var fork = "rucocohero";
 
     // Define our function responsible for extending the bot.
     function extend() {
@@ -46,12 +46,12 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
+      botName: "Kollgah der BOT",
       language: "english",
       chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
-      startupCap: 1, // 1-200
+      startupCap: 50, // 1-200
       startupVolume: 0, // 0-100
       startupEmoji: false, // true or false
       autowoot: true,
