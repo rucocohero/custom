dc00aa4d1fe00f443de9ddad5761e4efe1070220
rucocohero <cocoru@gmx.de>
2017-06-02 06:14:09 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -59,7 +59,7 @@
       smartSkip: true,
       cmdDeletion: true,
       maximumAfk: 45,
-      afkRemoval: true,
+      afkRemoval: false,
       maximumDc: 60,
       bouncerPlus: true,
       blacklistEnabled: true,
